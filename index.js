6839aeafab46cedd4c3bd85dfea46c3a6e5dd2e9
Atanu Sarkar <mratanusarkar@gmail.com>
2022-03-25 12:22:42 +0530

    added feature to resume process from a ith iteration

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const progressBar = require("./myProgressBar");
 // Main for testing the Progress Bar!
 let loop_len = 100;
 let counter = 0;
+let resumeFrom = 30;
 let startTime = new Date();
 
 for (i = 0; i < loop_len; ++i) {
     counter = progressBar.myProgressBar(counter, loop_len, startTime);
+
+    if (counter < resumeFrom) {
+        console.log("> skipping", counter);
+        continue;
+    }
 
     // do some time consuming task in loop
     var waitTill = new Date(new Date().getTime() + 100);
